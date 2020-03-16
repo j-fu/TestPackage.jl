95e1d3108379c22a226ae99ece84db696e993453
Juergen Fuhrmann <juergen.fuhrmann@wias-berlin.de>
2020-03-17 00:08:14 +0100

    Version 0.2.0

--- src/TestPackage.jl
+++ src/TestPackage.jl
@@ -1,5 +1,10 @@
-module TestPackage
+module TestPackage    
+using AbstractTrees
+using InteractiveUtils
+AbstractTrees.children(x::Type) = subtypes(x)
+numbertree()=AbstractTrees.print_tree(Number)
 
-greet() = print("Hello World!")
+
+greet() = println("Hello Universe!")
 
 end # module
